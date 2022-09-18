6a269b4a2c16effeda4f978c875583d0641c559c
Matthew Davis <matthewdavis1372@yahoo.com>
2022-09-18 00:03:27 -0400

    10

--- src/index.jsx
+++ src/index.jsx
@@ -4,11 +4,11 @@ import App from './App';
 import './style.css'
 
 
-ReactDOM.render(
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </React.StrictMode>
 );
 
 
