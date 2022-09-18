820309ce255919947fd3791d9cb49a3053e0068b
Matthew Davis <matthewdavis1372@yahoo.com>
2022-09-18 01:01:28 -0400

    reverting and got rid of slash at beginning of route

--- src/components/PortfolioContainer.jsx
+++ src/components/PortfolioContainer.jsx
@@ -3,13 +3,7 @@ import NavTabs from './NavTabs';
 import About from './pages/about';
 import Projects from './pages/projects';
 import Contact from './pages/contact';
-<<<<<<< HEAD:src/components/PortfolioContainer.jsx
-import ErrorPage from './pages/404'
-// import Footer from './Footer'
-import { HashRouter as Router, Routes, Route } from 'react-router-dom';
-=======
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
->>>>>>> parent of 87c60f9 (added footer, wont render it for now for formatting reasons. app ready to go :)):Client/src/components/PortfolioContainer.js
 
 
 export default function PortfolioContainer() {
@@ -20,9 +14,9 @@ export default function PortfolioContainer() {
         <main>
           <Routes>
             {/* <Route path='/' element={<About />} />   */}
-            <Route path='/About' element={<About />} />
-            <Route path='/Project' element={<Projects />} />
-            <Route path='/Contact' element={<Contact />} />
+            <Route path='About' element={<About />} />
+            <Route path='Project' element={<Projects />} />
+            <Route path='Contact' element={<Contact />} />
             {/* <Route path='*' element={<ErrorPage />} /> */}
           </Routes>
         </main>
